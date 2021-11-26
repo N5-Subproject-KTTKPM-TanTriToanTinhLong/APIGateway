a2e83f9f274f58b2c7ae343a371552f783250309
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-11-26 08:53:40 +0700

    Add APIGateway

---
+++ src/main/java/com/example/n5_apigateway/N5ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.n5_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N5ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N5ApiGatewayApplication.class, args);
+    }
+
+}
